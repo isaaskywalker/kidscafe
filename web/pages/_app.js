21085d1b17bfebd12ff23ed78ff391234ccc5da8
Isabelle <belleyuri@gmail.com>
2025-06-07 18:18:33 +0900

    Delete web directory

--- web/pages/_app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import type { AppProps } from 'next/app';
-import '../styles/kakao.css';
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-} 
